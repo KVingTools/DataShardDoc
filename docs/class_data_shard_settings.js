ab3a49d39fad3bc11a57c12792d360946baa2353
KVingTools <kving.tools@gmail.com>
2025-04-29 14:34:18 +0200

    Fix some typos

--- docs/class_data_shard_settings.js
+++ docs/class_data_shard_settings.js
@@ -1,8 +1,8 @@
 var class_data_shard_settings =
 [
-    [ "GetCategoryName", "class_data_shard_settings.html#ad33dca17b1b0a4a9a01637f77a009a6c", null ],
-    [ "GetContainerName", "class_data_shard_settings.html#a2d08eda8110e4b5560a6a0226cdb1d76", null ],
-    [ "GetSectionName", "class_data_shard_settings.html#ad90b801fdb166c5becec45a256a8b13a", null ],
+    [ "GetCategoryName", "class_data_shard_settings.html#af7c1e228e4aef9e079be266f90632ed1", null ],
+    [ "GetContainerName", "class_data_shard_settings.html#ac6911af4d76af48ff0b08e44ac49e472", null ],
+    [ "GetSectionName", "class_data_shard_settings.html#a9cd8b57b58c716190c4df9eee279736f", null ],
     [ "UDataShardSettings", "class_data_shard_settings.html#a079701561caea701bf8be2d34936f84c", null ],
     [ "InitialPoolSize", "class_data_shard_settings.html#ac3ccb52b72ea391601bc688b7d59af41", null ]
 ];
